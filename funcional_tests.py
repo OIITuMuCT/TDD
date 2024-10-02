5d5dd7e99524bdcd2a4ed2ba473a2f830be13fd5
OIITuMuCT <vataman@live.ru>
2024-10-02 19:08:40 +0300

    test 2

--- funcional_tests.py
+++ funcional_tests.py
@@ -1,6 +1,25 @@
 from selenium import webdriver
+import unittest
+class NewVisitorTest(unittest.TestCase):
+    """тест нового посетителя"""
 
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
-assert "Django" in browser.title
-print('hello')
+    def setUp(self):
+        '''установка'''
+        self.browser = webdriver.Chrome()
+    def tearDown(self):qq
+        '''демонтаж'''
+        self.browser.quit()
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        '''тест: можно начать список и получить его позже'''
+        # Эдит слышала про крутое новое онлайн-приложение со
+        # списком неотложных дел. Она решает оценить его
+        # домашнюю страницу
+        self.browser.get('http://localhost:8000')
+        # Она видит, что заголовок и шапка страницы говорят о
+        # списках неотложных дел
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Закончить тест!')
+        # Ей сразу же предлагается ввести элемент списка
+        # [...остальные комментарии, как и прежде]
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
