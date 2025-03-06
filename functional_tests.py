16e2c329ca6d9bf089192ff0928a930e934ff7ff
OIITuMuCT <vataman@live.ru>
2025-03-06 16:16:44 +0300

    Next Django ORM и первая модель

--- functional_tests.py
+++ functional_tests.py
@@ -25,6 +25,12 @@ class NewVisitorTest(unittest.TestCase):
         """ демонтаж """
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        """ подтверждение строки в таблице списка """
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         """тест: можно начать список и получить его позже """
         # Эдит слышала про крутое новое онлайн-приложение со
@@ -47,18 +53,23 @@ class NewVisitorTest(unittest.TestCase):
 
         # Она набирает в текстовом поле "Купить павлиньи перья"
         # (ее хобби – вязание рыболовных мушек)
-        # inputbox.send_keys('Купить павлиньи перья')
-        inputbox.send_keys("Сделать мушку из павлиньих")
-
+        inputbox.send_keys('Купить павлиньи перья')
         # Когда она нажимает enter, страница обновляется, и теперь страница
         # содержит "1: Купить павлиньи перья" в качестве элемента списка
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table('1: Купить павлиньи перья')
+        # Текствое поле по-прежнему приглашает ее добавить еще один элемент.
+        # Она вводит "Сделать мушку из павлиньих перьев"
+        # (Эдит очень методична)
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        inputbox.send_keys('Сделать мушку из павлиньих перьев')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
-        table = self.browser.find_element(By.ID, "id_list_table")
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
-        self.assertIn('2: Сделать мушку из павлиньих перьев', [row.text for row in rows])
+        # Страница снова обновляется и теперь показывает оба элемента ее списка
+        self.check_for_row_in_list_table("1: Купить павлиньи перья")
+        self.check_for_row_in_list_table("2: Сделать мушку из павлиньих перьев")
         # self.assertTrue(
         #     any(row.text == '1: Купить павлиньи перья' for row in rows),
         #     f"Новый элемент списка не появился в таблице. Содержимое было:\n{table.text}"
