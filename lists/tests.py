131bfcb0bdec267962cce5fdaa9040bdf50329cc
OIITuMuCT <vataman@live.ru>
2025-03-06 22:09:58 +0300

    Промежуточный коммит

--- lists/tests.py
+++ lists/tests.py
@@ -26,7 +26,7 @@ class HomePageTest(TestCase):
 
         response = self.client.post("/", data={"item_text": "A new list item"})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/единственный-в-своем-роде-список-в-мире/')
 
         # self.assertIn('A new list item', response.content.decode())
         # self.assertTemplateUsed(response, 'home.html')
@@ -78,3 +78,15 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
+
+class ListViewTest(TestCase):
+    """ тест представления списка """
+    def test_display_all_items(self):
+        """ тест: отображаются все элементы списка """
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+        
+        response = self.client.get('lists/один-единственный-список-в-мире/')
+        
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
--- superlist/urls.py
+++ superlist/urls.py
@@ -23,5 +23,6 @@ from lists import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', views.home_page, name='home'),
+    path('lists/один-единственный-список-в-мире/', views.view_list, name='view_list'),
 
 ]
