d0874d2d72134872527e8b1d7a4cd8637095af9b
OIITuMuCT <vataman@live.ru>
2025-03-06 19:16:04 +0300

    Модель для элементов списка и связанная с ней миграция

--- lists/tests.py
+++ lists/tests.py
@@ -2,7 +2,7 @@ from django.template.loader import render_to_string
 from django.urls import resolve
 from django.test import TestCase
 
-
+from lists.models import Item
 from lists.views import home_page
 
 # Create your tests here.
@@ -25,13 +25,33 @@ class HomePageTest(TestCase):
         домашней страницы"""
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
     def test_home_page_returns_correct_html(self):
         """ тест: домашняя страница возвращает правильный html  """
         response = self.client.get('/')
         html = response.content.decode('utf8')
-        expected_html = render_to_string('home.html')
-        self.assertEqual(html, expected_html)
+        # expected_html = render_to_string('home.html')
+        # self.assertEqual(html, expected_html)
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', html)
         self.assertTrue(html.strip().endswith('</html>'))
         self.assertTemplateUsed(response, 'home.html')
+
+class ItemModelTest(TestCase):
+    """ тест модели элемента списка """
+    def test_saving_and_retrieving_itmes(self):
+        """ тест сохранения и получения элементов списка """
+        first_item = Item()
+        first_item.text = "The first (ever) list item"
+        first_item.save()
+        
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+        
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
