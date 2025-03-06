26831d54882bcc0130ea787aa94197655008f9c6
OIITuMuCT <vataman@live.ru>
2025-03-06 20:07:40 +0300

    Переадресация после POST и показ всех элемнтов в шаблоне

--- lists/tests.py
+++ lists/tests.py
@@ -16,10 +16,21 @@ class HomePageTest(TestCase):
 
     def test_can_save_a_POST_request(self):
         """ тест: можно сохранить post-запрос """
-        response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertIn('A new list item', response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')
-    
+        self.client.post("/", data={"item_text": "A new list item"})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirect_arter_POST(self):
+        """тест: переадресует после post-запроса"""
+
+        response = self.client.post("/", data={"item_text": "A new list item"})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+        # self.assertIn('A new list item', response.content.decode())
+        # self.assertTemplateUsed(response, 'home.html')
+
     def test_root_url_resolves_to_home_page_view(self):
         """ тест: корневой url преобразуется в представление
         домашней страницы"""
@@ -37,9 +48,21 @@ class HomePageTest(TestCase):
         self.assertTrue(html.strip().endswith('</html>'))
         self.assertTemplateUsed(response, 'home.html')
 
+    def test_only_saves_items_when_necessary(self):
+        """ тест сохраняет элементы, только когда нужно """
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+    def test_display_all_list_items(self):
+        """ тест: отображаются все элементы списка """
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+        response = self.client.get('/')
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 1', response.content.decode())
+
 class ItemModelTest(TestCase):
     """ тест модели элемента списка """
-    def test_saving_and_retrieving_itmes(self):
+    def test_saving_and_retrieving_items(self):
         """ тест сохранения и получения элементов списка """
         first_item = Item()
         first_item.text = "The first (ever) list item"
