8ec6e9b148f9d67b3e53bafcee0b296638381f6b
OIITuMuCT <vataman@live.ru>
2025-03-07 15:13:43 +0300

    Новый URL + представление для добавления в существующие  списки. ФТ проходит :-)

--- lists/tests.py
+++ lists/tests.py
@@ -2,7 +2,7 @@ from django.template.loader import render_to_string
 from django.urls import resolve
 from django.test import TestCase
 
-from lists.models import Item
+from lists.models import Item, List
 from lists.views import home_page
 
 # Create your tests here.
@@ -14,9 +14,61 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
+class ListViewTest(TestCase):
+    """ тест представления списка """
+
+    def test_uses_list_template(self):
+        """ тест: используется шаблон списка """
+        response = self.client.get('/lists/единственный-список-в-мире/')
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_display_all_items(self):
+        """ тест: отображаются все элементы списка """
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+        
+        response = self.client.get('lists/единственный-список-в-мире/')
+        
+        self.assertContains(response, 'itemey 1', list=list_)
+        self.assertContains(response, 'itemey 2', list=list_)
+        
+
+class ListAndItemModelTest(TestCase):
+    """ тест модели элемента списка """
+    def test_saving_and_retrieving_items(self):
+        """ тест сохранения и получения элементов списка """
+        list_ = List()
+        list_.save()
+
+        first_item = Item()
+        first_item.text = "Первый (самый) элемент списка"
+        first_item.list = list_
+        first_item.save()
+        
+        second_item = Item()
+        second_item.text = 'Элемент второй'
+        second_item.list = list_
+        second_item.save()
+        
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
+        
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, list_)
+        self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, list_)
+        
+
+class NewListTest(TestCase):
+    """ тест нового списка """
     def test_can_save_a_POST_request(self):
         """ тест: можно сохранить post-запрос """
-        self.client.post("/", data={"item_text": "A new list item"})
+        self.client.post("/lists/new", data={"item_text": "A new list item"})
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
@@ -24,69 +76,5 @@ class HomePageTest(TestCase):
     def test_redirect_arter_POST(self):
         """тест: переадресует после post-запроса"""
 
-        response = self.client.post("/", data={"item_text": "A new list item"})
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/единственный-в-своем-роде-список-в-мире/')
-
-        # self.assertIn('A new list item', response.content.decode())
-        # self.assertTemplateUsed(response, 'home.html')
-
-    def test_root_url_resolves_to_home_page_view(self):
-        """ тест: корневой url преобразуется в представление
-        домашней страницы"""
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
-    def test_home_page_returns_correct_html(self):
-        """ тест: домашняя страница возвращает правильный html  """
-        response = self.client.get('/')
-        html = response.content.decode('utf8')
-        # expected_html = render_to_string('home.html')
-        # self.assertEqual(html, expected_html)
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.strip().endswith('</html>'))
-        self.assertTemplateUsed(response, 'home.html')
-
-    def test_only_saves_items_when_necessary(self):
-        """ тест сохраняет элементы, только когда нужно """
-        self.client.get('/')
-        self.assertEqual(Item.objects.count(), 0)
-    def test_display_all_list_items(self):
-        """ тест: отображаются все элементы списка """
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-        response = self.client.get('/')
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 1', response.content.decode())
-
-class ItemModelTest(TestCase):
-    """ тест модели элемента списка """
-    def test_saving_and_retrieving_items(self):
-        """ тест сохранения и получения элементов списка """
-        first_item = Item()
-        first_item.text = "The first (ever) list item"
-        first_item.save()
-        
-        second_item = Item()
-        second_item.text = 'Item the second'
-        second_item.save()
-        
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(second_saved_item.text, 'Item the second')
-
-class ListViewTest(TestCase):
-    """ тест представления списка """
-    def test_display_all_items(self):
-        """ тест: отображаются все элементы списка """
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-        
-        response = self.client.get('lists/один-единственный-список-в-мире/')
-        
-        self.assertContains(response, 'itemey 1')
-        self.assertContains(response, 'itemey 2')
+        response = self.client.post("/lists/new", data={"item_text": "A new list item"})
+        self.assertRedirects(response, '/lists/единственный-список-в-мире/')
--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 
-from lists.models import Item
+from lists.models import Item, List
 # Create your views here.
 
 def home_page(request):
@@ -10,10 +10,15 @@ def home_page(request):
         Item.objects.create(text=request.POST['item_text'])
         return redirect('/lists/один-единственный-список-в-мире/')
     
-    items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'home.html')
 
 def view_list(request):
     """ представление списка """
     items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'list.html', {'items': items})
+
+def new_list(request):
+    """ новый список """
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect('/lists/единственный-список-в-мире/')
