9f3447b132ec22e96c8b5a3d08d55470943fbb1d
OIITuMuCT <vataman@live.ru>
2025-03-10 15:22:47 +0300

    Завершение ФТ

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,28 +1,48 @@
 from .base import FunctionalTest
-# from selenium import webdriver
-# from selenium.webdriver.chrome.service import Service
-# from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
+# service = Service(executable_path="/snap/bin/geckodriver")
+service = Service(
+    executable_path="/home/neko/Documents/Projects/TDD/chromedriver-linux64/chromedriver"
+)
+browser = webdriver.Chrome(service=service)
 
 class ItemValidationTest(FunctionalTest):
     """ тест валидации элемента списка """
 
-
     def test_cannot_add_empty_list_items(self):
         """ тест: нельзя добавлять пустые элементы списка"""
-        # Эдит открывает домашнюю страницу и случайно пытается отправить 
+        # Эдит открывает домашнюю страницу и случайно пытается отправить
         # пустой элемент списка. Она нажимает Enter на пустом поле ввода
+        self.browser.get(self.live_server_url)
+        self.browser.find_element(By.ID, 'id_new_item').send_keys(Keys.ENTER)
 
-        # Домашняя страница обновляется, и появляется сообщение об ошибке, 
+        # Домашняя страница обновляется, и появляется сообщение об ошибке,
         # которое говорит, что элементы списка не должны быть пустыми
-
-        # Она пробует снова, теперь с неким текстом для элемента, и теперь 
+        self.wait_for(lambda: self.assertEqual(
+        self.browser.find_element(By.CSS_SELECTOR,'.has-error').text,
+        "You can't have an empty list item"
+        ))
+        # Она пробует снова, теперь с неким текстом для элемента, и теперь
         # это срабатывает
+        self.browser.find_element(By.ID, 'id_new_item').send_keys('Купить молоко')
+        self.browser.find_element(By.ID, 'id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Купить молоко')
 
         # Как ни странно, Эдит решает отправить второй пустой элемент списка
+        self.browser.find_element(By.ID, 'id_new_item').send_keys(Keys.ENTER)
         # Она получает аналогичное предупреждение на странице списка
+        self.wait_for(lambda: self.assertEqual(
+        self.browser.find_element(By.CSS_SELECTOR, '.has-error').text,
+        "You can't have an empty list item"
+        ))
 
         # И она может его исправить, заполнив поле неким текстом
-        self.fail('Напиши меня!')
-
+        self.browser.find_element(By.ID, 'id_new_item').send_keys('Make tea')
+        self.browser.find_element(By.ID, 'id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Купить молоко')
+        self.wait_for_row_in_list_table("2: Make tea")
+        self.fail('Закончить тест!')
