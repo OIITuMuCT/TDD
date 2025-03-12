d3fd56ffc5a6605dc3d046d3c0bc92106aa6bf0a
OIITuMuCT <vataman@live.ru>
2025-03-12 22:41:07 +0300

    Добавлены изменения в модель User models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    """ пользователь """
+    email = models.EmailField(unique=True)
+    REQUIRED_FIELDS = []
+    USERNAME_FIELD = 'email'
+    is_anonymous = False
+    is_authenticated = True
+    
---
+++ accounts/tests/test_models.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class UserModelTest(TestCase):
+    """ тест модели пользователя """
+    def test_user_is_valid_with_email_only(self):
+        """ тест: пользователь допустим только с электронной почтой """
+        user = User(email='a@b.com')
+        user.full_clean() # не должно поднять исключение
+    
