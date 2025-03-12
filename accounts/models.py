2d29646c081d9d48731addf4b928128550d3cf4a
OIITuMuCT <vataman@live.ru>
2025-03-12 22:18:40 +0300

    New accaunt startapp

--- accounts/models.py
+++ accounts/models.py
@@ -1,37 +1,3 @@
-from django.contrib.auth.models import (
-    AbstractBaseUser, BaseUserManager, PermissionsMixin
-)
 from django.db import models
+
 # Create your models here.
-class Token(models.Model):
-    """ маркер """
-    email = models.EmailField()
-    uid = models.CharField(max_length=255)
-
-class ListUserManager(BaseUserManager):
-    """менеджер пользователя списка"""
-
-    def create_user(self, email):
-        """создать пользователя"""
-        ListUser.objects.create(email=email)
-    
-    def create_superuser(self, email, password):
-        """ создать суперпользователя"""
-        self.create_user(email)
-
-class ListUser(AbstractBaseUser, PermissionsMixin):
-    """Пользователь списка"""
-    email = models.EmailField(primary_key=True)
-    USERNAME_FIELD = 'email'
-    #REQUIRED_FIELDS = ['email', 'height']
-    
-    objects = ListUserManager()
-    
-    @property
-    def is_staff(self):
-        return self.email == 'harry.persival@example.com'
-    
-    @property
-    def is_active(self):
-        return True
-
--- superlist/urls.py
+++ superlist/urls.py
@@ -20,9 +20,11 @@ from django.contrib import admin
 from django.urls import path, re_path
 from lists import views as list_views
 from lists import urls as list_urls
-
+from accounts import urls as accounts_urls
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     re_path(r"^$", list_views.home_page, name="home"),
     re_path(r"^lists/", include(list_urls)),
+    re_path(r'^accounts/', include(accounts_urls))
 ]
